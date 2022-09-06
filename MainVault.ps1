719fd18132fc29f15ce14d13fb0f62178e7134ec
dru0pa <dru0pa@gmail.com>
2022-09-06 16:02:15 +0200

    Update MainVault.ps1

--- MainVault.ps1
+++ MainVault.ps1
@@ -3,3 +3,5 @@ Add-Content "C:\PowerShell\CyberArk Services\output.txt" 'Vault01'
 Get-Service *Ark* | Out-File -FilePath "C:\PowerShell\CyberArk Services\output.txt" -Append
 Get-CimInstance win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberArk Services\output.txt" -Append
 Get-CimInstance -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberArk Services\output.txt" -Append
+Add-Content "C:\PowerShell\CyberArk Services\output.txt" 'italog.log'
+Get-content -tail 10 "C:\Program Files (x86)\PrivateArk\Server\Logs\italog.log" | Out-File -FilePath "C:\PowerShell\CyberArk Services\output.txt" -Append
