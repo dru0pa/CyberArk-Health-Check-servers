e25e35b6d75ef4cbb0128bf55c9e0148d449c98b
dru0pa <dru0pa@gmail.com>
2024-10-11 12:39:33 +0200

    Update Sevives.ps1

--- Sevives.ps1
+++ Sevives.ps1
@@ -1,79 +1,66 @@
-Get-Date| Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt"
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'PVWA'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PVWA01>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PVWA01> *Ark*, IISADMIN | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PVWA01> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PVWA01> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PVWA01> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PVWA02>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PVWA02> *Ark*, IISADMIN | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PVWA02> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PVWA02> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PVWA02> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPM'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<CPM01>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <CPM01> -Name *Ark* | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <CPM01> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <CPM01> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <CPM01> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Get-Date| Out-File -FilePath "C:\PowerShell\CyberArk Services\outputpadr.txt"
-Add-Content "C:\PowerShell\CyberArk Services\outputpadr.txt" 'PAReplicate.log'
-Get-content -tail 70 "C:\Program Files (x86)\PrivateArk\Replicate\PAReplicate.log" | Out-File -FilePath "C:\PowerShell\CyberArk Services\outputPAReplicate.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<CPM02>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <CPM02> -Name *Ark* | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <CPM02> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <CPM02> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <CPM02> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'PSM'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PSM01>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PSM01> -Name *Ark*  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PSM01> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PSM01> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PSM01> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PSM02>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PSM02> -Name *Ark* | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PSM02> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PSM02> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PSM02> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PSM03>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PSM03> -Name *Ark* | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PSM03> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PSM03> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PSM03> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" '<PSM04>'
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CyberArk Services'
-Get-Service -ComputerName <PSM04> -Name *Ark* | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'CPU LoadPercentage'
-Get-CimInstance -ComputerName <PSM04> win32_processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'Disk Free Space'
-Get-CimInstance -ComputerName <PSM04> -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID,@{'Name' = 'FreeSpace (GB)'; Expression= { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
-Add-Content "C:\PowerShell\CyberARk Services\output.txt" 'RAM'
-Get-WmiObject -Class win32_operatingsystem -ComputerName <PSM04> | ft @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} -AutoSize  | Out-File -FilePath "C:\PowerShell\CyberARk Services\output.txt" -Append
+# Define the output file path
+$outputFile = "C:\PowerShell\CyberARk Services\output.txt"
+
+# Define an array of server objects
+$servers = @(
+    @{
+        Name = 'PVWA'
+        Computers = @(
+            'PVWA-01' # Updated computer name
+ #           '<PVWA02>'
+        )
+        ServiceNamePattern = '*Ark*', 'IISADMIN'
+    },
+    @{
+        Name = 'CPM'
+        Computers = @(
+            'CPM-01' # Updated computer name
+ #           '<CPM02>' # Updated computer name
+        )
+        ServiceNamePattern = '*Ark*'
+    },
+    @{
+        Name = 'PSM'
+        Computers = @(
+            'PSM-01' # Updated computer name
+ #           '<PSM02>' # Updated computer name
+ #           '<PSM03>' # Updated computer name
+ #           '<PSM04>' # Updated computer name
+        )
+        ServiceNamePattern = '*Ark*'
+    }
+)
+
+# Clear the output file
+Clear-Content -Path $outputFile
+
+# Get the current date and time
+Get-Date | Out-File -FilePath $outputFile
+
+# Loop through each server object
+foreach ($server in $servers) {
+    Add-Content -Path $outputFile -Value $server.Name
+
+    # Loop through each computer in the server object
+    foreach ($computer in $server.Computers) {
+        Add-Content -Path $outputFile -Value $computer
+        Add-Content -Path $outputFile -Value 'CyberArk Services'
+        Get-Service -ComputerName $computer -Name $server.ServiceNamePattern | Out-File -FilePath $outputFile -Append
+        Add-Content -Path $outputFile -Value 'CPU LoadPercentage'
+        Get-CimInstance -ComputerName $computer Win32_Processor | Measure-Object -Property LoadPercentage -Average | Out-File -FilePath $outputFile -Append
+        Add-Content -Path $outputFile -Value 'Disk Free Space'
+        Get-CimInstance -ComputerName $computer -ClassName Win32_LogicalDisk | Select-Object -Property DeviceID, @{Name = 'FreeSpace (GB)'; Expression = { [int]($_.FreeSpace / 1GB) }} | Out-File -FilePath $outputFile -Append
+        Add-Content -Path $outputFile -Value 'RAM'
+
+        # Use Get-CimInstance for RAM information
+        Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $computer | 
+        Select-Object -Property @{Name="Total Visible Memory Size (GB)";e={[math]::truncate($_.TotalVisibleMemorySize /1MB)}}, @{Name="Free Physical Memory (GB)";e={[math]::truncate($_.FreePhysicalMemory /1MB)}} | 
+        Out-File -FilePath $outputFile -Append
+
+    }
+}
+
+# Add PAReplicate.log content to the output file
+# Edit the 70 to make sure the log will show the start of the last backup
+Add-Content -Path $outputFile -Value 'PAReplicate.log'
+Get-Content -Tail 70 "C:\Program Files (x86)\PrivateArk\Replicate\PAReplicate.log" | Out-File -FilePath $outputFile -Append
